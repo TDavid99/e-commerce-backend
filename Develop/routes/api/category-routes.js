82772a27731f0f419f7b8508bdd5227cc4a9d6de
townsendda@sandhills.edu <108563194+TDavid99@users.noreply.github.com>
2022-10-11 11:24:35 -0400

    working in routes category

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,15 +6,44 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try{
+    const categoriesItem = await Category.findAll({
+      include: [{model: Product}],
+    });
+    res.status(200).json(categoriesData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try { 
+    const categoriesItem = await Category.findByPk(req.params.id,{
+      include: [{model: Product}],
+    });
+    if(!categoriesItem) {
+      res.status(404).json({message: "No category found with that id!"});
+      return;
+    }
+    res.status(200).json(categoriesItem);
+
+  }catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  try {
+    const categoriesItem = await Category.create({
+      category_title: req.body.category_title,
+    });
+    res.status(200).json(categoriesItem);
+  }catch(err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
