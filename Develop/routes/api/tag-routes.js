8f42be37747c4694ca1bb99431bae2797e494d0c
townsendda@sandhills.edu <108563194+TDavid99@users.noreply.github.com>
2022-10-12 18:46:28 -0400

    tagdata

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -1,24 +1,58 @@
 const router = require('express').Router();
+const { json } = require('sequelize/types');
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const TagItem = await Tag.findAll ({
+      include: [{model: Product}],
+    });
+    res. status(200).json(tagsItem);
+  }catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const TagItem = await Tag.findByPk(req.params.id,{
+    include: [{model: Product}],
+  });
+  if (!tagItem){
+    res.status(404).json({message: "No tag found wtih that id."});
+    return;
+  }
+  res. status(200).json(tagItem);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+
+
+router.post('/',async (req, res) => {
   // create a new tag
+  try {
+    const TagItem = await Tag.creat({
+      TagItem: req.body.tag_name,
+    });
+    res.status(200).json(TagItem);
+  }catch (err) {
+    res. status(400).json(err)
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    
+  }
 });
 
 router.delete('/:id', (req, res) => {
